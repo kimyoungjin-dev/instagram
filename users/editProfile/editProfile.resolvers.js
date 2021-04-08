aa142896811e39a54b2bf8ad079095eb1c29956e
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-08 22:04:08 +0900

    Modify editProfile.resolvers.js
    Add bio, bio

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,18 +1,28 @@
 import bcrypt from "bcrypt";
 import client from "../../client";
-import { protectUser } from "../users.utils";
+import { protectedResolver } from "../users.utils";
+import { createWriteStream } from "fs";
 
 const resolverFn = async (
   _,
-  { firstName, lastName, username, email, password: newPassword, bio },
-  { loggedInUser, protectUser }
+  { firstName, lastName, username, email, password: newPassword, bio, avatar },
+  { loggedInUser }
 ) => {
+  const { filename, createReadStream } = await avatar;
+  const readStream = createReadStream();
+  const writeStream = createWriteStream(process.cwd() + "/uploads/" + filename);
+  readStream.pipe(writeStream);
+
+  console.log(stream);
+
   let uglyPassword = null;
   if (newPassword) {
     uglyPassword = await bcrypt.hash(newPassword, 10);
   }
   const updatedUser = await client.user.update({
-    where: { id: loggedInUser.id },
+    where: {
+      id: loggedInUser.id,
+    },
     data: {
       firstName,
       lastName,
@@ -28,13 +38,13 @@ const resolverFn = async (
   } else {
     return {
       ok: false,
-      error: "수정하는 과정에서 오류가 발생하였습니다.",
+      error: "Could not update profile.",
     };
   }
 };
 
 export default {
   Mutation: {
-    editProfile: protectUser(resolverFn),
+    editProfile: protectedResolver(resolverFn),
   },
 };
