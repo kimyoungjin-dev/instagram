47da19af39b805316c2528395a1b334de709ebb4
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-08 19:18:57 +0900

    Modify mutations.js
    Checks whether an account exists as existringUser.
    
    Create uglyPassword to hash the password.
    
    login part: Check if there is an account with checkUser. Make sure checkPassword is correct. In both cases, if it is true, a token is given.

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,5 +1,6 @@
 import client from "../client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
@@ -7,28 +8,62 @@ export default {
       _,
       { firstName, lastName, username, email, password }
     ) => {
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [
-            {
-              username,
-            },
-            {
-              email,
-            },
-          ],
-        },
-      });
-      const uglyPassword = await bcrypt.hash(password, 10);
-      return client.user.create({
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          password,
-        },
-      });
+      try {
+        const existringUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existringUser) {
+          throw new Error("이미 계정이 존재합니다");
+        }
+        const uglyPassword = await bcrypt.hash(password, 10);
+        return client.user.create({
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            password: uglyPassword,
+          },
+        });
+      } catch (error) {
+        return error;
+      }
+    },
+
+    login: async (_, { username, password }) => {
+      const checkUser = await client.user.findFirst({ where: { username } });
+      if (!checkUser) {
+        return {
+          ok: false,
+          error: "유저를 찾을수가없습니다.",
+        };
+      }
+
+      const checkPassword = await bcrypt.compare(password, checkUser.password);
+      if (!checkPassword) {
+        return {
+          ok: false,
+          error: "패스워드가 틀렸습니다.",
+        };
+      }
+
+      const token = await jwt.sign(
+        { id: checkUser.id },
+        process.env.SECRET_KEY
+      );
+      return {
+        ok: true,
+        token,
+      };
     },
   },
 };
