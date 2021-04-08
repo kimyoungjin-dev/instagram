4073365e942eacfeb20fb69b3a06346b8d4e9250
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-08 15:47:12 +0900

    Modify the server.js file
    Create server.js and updateMovie.

--- server.js
+++ server.js
@@ -12,21 +12,25 @@ const typeDefs = gql`
     createdAt: String!
     updatedAt: String!
   }
+
   type Query {
     movies: [Movie]
     movie(id: Int!): Movie
   }
+
   type Mutation {
     createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id: String!): Boolean
+    deleteMovie(id: Int!): Movie
+    updateMovie(id: Int!, year: Int!): Movie
   }
 `;
 
 const resolvers = {
   Query: {
     movies: () => client.movie.findMany(),
-    movie: (_, { id }) => ({ title: "Hello", year: 2021 }),
+    movie: (_, { id }) => client.movie.findUnique({ where: { id } }),
   },
+
   Mutation: {
     createMovie: (_, { title, year, genre }) =>
       client.movie.create({
@@ -36,15 +40,17 @@ const resolvers = {
           genre,
         },
       }),
-    deleteMovie: (_, { id }) => {
-      return true;
-    },
+    deleteMovie: (_, { id }) => client.movie.delete({ where: { id } }),
+    updateMovie: (_, { id, year }) =>
+      client.movie.update({ where: { id }, data: { year } }),
   },
 };
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
 server
   .listen()
   .then(() => console.log("Server is running on http://localhost:4000/"));
