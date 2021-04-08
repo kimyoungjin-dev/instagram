98c6bcd3c0b6f58911e88c21199374324e4d53d9
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-09 02:26:36 +0900

    Write code to get the array of follower users.

--- users/seeFollowers/seeFollowers.resolvers.js
+++ users/seeFollowers/seeFollowers.resolvers.js
@@ -3,12 +3,19 @@ import client from "../../client";
 export default {
   Query: {
     seeFollowers: async (_, { username, page }) => {
+      const ok = await client.user.findUnique({
+        where: { username },
+        select: { id: true },
+      });
+
       const followers = await client.user
         .findUnique({ where: { username } })
+
         .followers({
           take: 5,
           skip: (page - 1) * 5,
         });
+
       const totalFollowers = await client.user.count({
         where: { following: { some: { username } } },
       });
