378600c7c963795f06a1c9907cd2a1bd58ed4330
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-09 23:44:42 +0900

    Following checks how many my IDs are on other people's followers list.  The follower checks how many my IDs are on the other's following list.
    totalFollowing: The number of people I followed
    
    totalFollowers: number of people who applied to follow me

--- users/users.resolvers.js
+++ users/users.resolvers.js
@@ -1,8 +1,29 @@
+import client from "../client";
+
 export default {
   User: {
-    totalFollowing: (root) => {
-      console.log(root.email);
-      return 0;
+    totalFollowing: ({ id }) => {
+      return client.user.count({
+        where: {
+          followers: {
+            some: {
+              id,
+            },
+          },
+        },
+      });
+    },
+
+    totalFollowers: ({ id }) => {
+      return client.user.count({
+        where: {
+          following: {
+            some: {
+              id,
+            },
+          },
+        },
+      });
     },
   },
 };
