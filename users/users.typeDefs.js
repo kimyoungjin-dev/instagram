fd122857b6d7062f33d39b4086043af4445d6490
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-09 22:26:36 +0900

    Add totalFollwoing and totalFollwers to type User

--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -13,5 +13,10 @@ export default gql`
     avatar: String
     followers: [User]
     following: [User]
+    totalFollowing: Int!
+    totalFollowers: Int!
   }
 `;
+
+// isFollowing: Boolean!
+// isMe: Boolean!
