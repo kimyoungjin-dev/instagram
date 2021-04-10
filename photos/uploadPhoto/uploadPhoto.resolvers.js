9fe6bebad2de4ab7ed40d568c6989a90848539c6
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-10 23:59:58 +0900

    connectOrCreate
    import client
    
    If #hashtag exists, use "where", and if you need to create a new one, use "create".

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,5 +1,5 @@
 import { protectedResolver } from "../../users/users.utils";
-
+import client from "../../client";
 //parsing : 캡션 문장안에 해쉬태그 키워드들만 추출해내는것
 //parsing 작업 => "caption"이 존재 + caption에 "해쉬테그" 존재 => Hashtages를 생성 => 해당 "hashtag"가 존재하는지 확인하고 => "get" or "create" 작업 => 마지막에 parsing 된 해쉬테크와 함께 "저장"=> 사진을 "hashtags" 에도 "저장"
 export default {
@@ -9,8 +9,20 @@ export default {
         //Regular Expression: 문자내에서 특정 문자를 추출한다.
         if (caption) {
           const hashtag = caption.match(/#[\w]+/g);
-          console.log(hashtag);
         }
+        client.photo.create({
+          data: {
+            file,
+            caption,
+            hashtags: {
+              //connectOrCreate 배열로 만들수있다.
+              connectOrCreate: [
+                //만약#food 가 존재를 한다면 where을 사용하고, 새로생성 해야된다면 create를 사용한다.
+                { where: { hashtag: "#food" }, create: { hashtag: "#food" } },
+              ],
+            },
+          },
+        });
       }
     ),
   },
