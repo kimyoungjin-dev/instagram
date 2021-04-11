043f2ed68d0b20afa33e471df13c945d3d1e40ca
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-11 22:06:33 +0900

    If you change the caption, Hashtech will fix the remaining bug.
    const ok => findFirst
    
    First, use include to include hashtags in prevPhoto
    
    Using the client.photo.update function
    
    Unique value: where: id
    
    data to hashtags. Disconnect prevPhoto's existing hashtech using disconnect.
    
    Use connectOrCreate to put a hash tag in the new caption.

--- src/photos/editPhoto/editPhoto.resolvers.ts
+++ src/photos/editPhoto/editPhoto.resolvers.ts
@@ -1,31 +1,53 @@
 import { protectedResolver } from "../../users/users.utils";
+import { processHashtags } from "../photo.utils";
 
-//1.수정 하고자 하는 사진을 찾는다. =>
+//1.수정 하고자 하는 사진을 찾는다. => 새로운 캡션에서 #를 추출 => connect => disconnect 로 끊는다 =>
 export default {
   Mutation: {
     editPhoto: protectedResolver(
       //id =플레이그라운드에 입력하는 아이디
       async (_, { id, caption }, { loggedInUser, client }) => {
-        const ok = await client.photo.findFirst({
+        const prevPhoto = await client.photo.findFirst({
           where: {
             id,
             userId: loggedInUser.id,
           },
+          //prevPhoto에는 hashtag가 존재하지않으므로 include 속성을 사용해서 hashtag를 불러온다
+          include: {
+            hashtags: {
+              select: {
+                hashtag: true,
+              },
+            },
+          },
         });
+        //prevPhoto는 기존의 hashtag를 가져올수있다.
+
         //만약 사진의 사진을 보유하고있는 userId 와 loggedInUser 의 id가 일치하지않으면 일치하지않는다는 애러를 return 한다.
-        if (!ok) {
+        if (!prevPhoto) {
           return {
             ok: false,
             error: "Login user and current user do not match.",
           };
         }
 
-        const photo = await client.photo.update({
+        await client.photo.update({
           //하나의 사진의 고유한 id가 존재하고 해당아이디의 사진의 caption 만을 수정한다.
           where: { id },
-          data: { caption },
+          data: {
+            caption,
+            //prevPhoto.hashtags를 사용하면 모든 해쉬테크를 disconnect 할수있다.
+            hashtags: {
+              //연결 을 취소한후에
+              disconnect: prevPhoto.hashtags,
+              //연결을 다시 새로운 해쉬테크를 만들어준다.
+              connectOrCreate: processHashtags(caption),
+            },
+          },
         });
-        console.log(photo);
+        return {
+          ok: true,
+        };
       }
     ),
   },
