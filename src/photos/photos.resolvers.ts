4a990005926c59f6f8a0bc956c3d5c74fe597a15
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-11 18:30:21 +0900

    Update photos.resolvers.ts
    You can use the page as args in the photos of the hashtag.
    
    By creating totalPhotos, you can find out the total number of photos with the hashtag you have chosen.

--- src/photos/photos.resolvers.ts
+++ src/photos/photos.resolvers.ts
@@ -2,6 +2,7 @@ import { Resolvers } from "../types";
 
 //computied fields 를 작성할때에는 root를 명시해주어야한다.
 const resolvers: Resolvers = {
+  //photo 를 위한 resolver
   Photo: {
     user: ({ userId }, _, { client }) =>
       client.user.findUnique({ where: { id: userId } }),
@@ -10,6 +11,20 @@ const resolvers: Resolvers = {
     //해쉬테그를 찾는다. 찾은 사진의 id를 가지고있는 hash tag이다. , photos해시태그들 내에서 찾는다. =>
     hashtags: ({ id }, _, { client }) =>
       client.hashtag.findMany({ where: { photos: { some: { id } } } }),
+  },
+
+  //hashtag 를 위한 resolver
+  //root는 우리가 찾는 사진 =>
+  Hashtag: {
+    //해당 해쉬테그에 해당하는 모든 사진을 불러올수있다. //하지만 사진이 많이존재한다면, 데이터베이스에 과부하가 생긴다.
+    photos: ({ id }, { page }, { client }) => {
+      return client.hashtag.findUnique({ where: { id } }).photos();
+    },
+    //"#food"라는 해쉬태그가 몇장에 사진에 포함되어있는지를 확인하는 코드
+    totalPhotos: ({ id }, _, { client }) =>
+      client.photo.count({
+        where: { hashtags: { some: { id } } },
+      }),
   },
 };
 
