9f6f5b024a8f88b250932b4f388dbe89d81a6629
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-12 04:08:17 +0900

    Write a comments resolver
    Shows the number of comments in the photo.

--- src/photos/photos.resolvers.ts
+++ src/photos/photos.resolvers.ts
@@ -9,6 +9,7 @@ const resolver: Resolvers = {
       //root의 id는 현재 내가찾고자하는 사진의 아이디이다. 이 아이디와 일치하는 아이디는 hashtag의 photos의 id 를 가지고있는 해쉬태그들만을 모아서 값을 보여준다.
       return client.hashtag.findMany({ where: { photos: { some: { id } } } });
     },
+
     //사진의 좋아요 갯수랑 세는 likes를 작성
     // photoId = photo id 와 같은 like 갯수를 센다.
     likes: ({ id }, _, { client }) => {
@@ -17,6 +18,12 @@ const resolver: Resolvers = {
         where: { photoId: id }, //11번의 아이디를 가지고있는 사진의 like 개수를 센다.
       });
     },
+
+    //사진의 "id"가 "comment"를 작성하는 사진의 id와 일치해야한다.
+    comments: ({ id }, __, { client }) =>
+      client.comment.count({
+        where: { photoId: id },
+      }),
   },
 
   Hashtag: {
