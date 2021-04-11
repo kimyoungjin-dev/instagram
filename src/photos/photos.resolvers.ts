90ca36eb6ec3c603b0ae99b290b54bb854566b98
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-11 19:00:48 +0900

    Change the content of the comment

--- src/photos/photos.resolvers.ts
+++ src/photos/photos.resolvers.ts
@@ -8,16 +8,16 @@ const resolvers: Resolvers = {
       client.user.findUnique({ where: { id: userId } }),
     //user의 id : Photo :userId
 
-    //해쉬테그를 찾는다. 찾은 사진의 id를 가지고있는 hash tag이다. , photos해시태그들 내에서 찾는다. =>
+    //해쉬테그를 찾는다. 찾은 사진의 id를 가지고있는 hashtag이다. , photos해시태그들 내에서 찾는다.
     hashtags: ({ id }, _, { client }) =>
       client.hashtag.findMany({ where: { photos: { some: { id } } } }),
   },
 
   //hashtag 를 위한 resolver
-  //root는 우리가 찾는 사진 =>
+  //root는 우리가 찾는 사진
   Hashtag: {
     //해당 해쉬테그에 해당하는 모든 사진을 불러올수있다. //하지만 사진이 많이존재한다면, 데이터베이스에 과부하가 생긴다.
-    photos: ({ id }, { page }, { client }) => {
+    photos: ({ id }, _, { client }) => {
       return client.hashtag.findUnique({ where: { id } }).photos();
     },
     //"#food"라는 해쉬태그가 몇장에 사진에 포함되어있는지를 확인하는 코드
