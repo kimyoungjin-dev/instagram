097368d2ad4c194787f55ee4393ebde2d6df7eff
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-12 23:56:50 +0900

    Component is protected in case of query as well as mutation by using the resolver function info.

--- src/users/users.utils.ts
+++ src/users/users.utils.ts
@@ -22,10 +22,16 @@ export const getUser = async (token) => {
 export function protectedResolver(ourResolver: Resolver) {
   return function (root, args, context, info) {
     if (!context.loggedInUser) {
-      return {
-        ok: false,
-        error: "수정 할 수 없습니다. 로그인 후 진행하세요",
-      };
+      const query = info.operation.operation === "query";
+      console.log(query);
+      if (query) {
+        return null;
+      } else {
+        return {
+          ok: false,
+          error: "수정 할 수 없습니다. 로그인 후 진행하세요",
+        };
+      }
     }
     return ourResolver(root, args, context, info);
   };
