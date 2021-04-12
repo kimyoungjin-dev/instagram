7839b7b19df17ddf885e43eef692b22d6c1ce74e
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-12 21:54:47 +0900

    If loggedInUser does not exist, return false

--- src/photos/seeFeed/seeFeed.resolvers.ts
+++ src/photos/seeFeed/seeFeed.resolvers.ts
@@ -2,12 +2,15 @@ import { protectedResolver } from "../../users/users.utils";
 
 //유저가 누구인지 알아야하기때문에 protectedResolver 사용
 
+//"photo"를 찾을때 나의 아이디가 팔로워 목록에있는 유저의 photo를 찾으면 된다.
 export default {
   Query: {
     seeFeed: protectedResolver(
-      async (_, { page }, { client, loggedInUser }) =>
-        //"photo"를 찾을때 나의 아이디가 팔로워 목록에있는 유저의 photo를 찾으면 된다.
-        await client.photo.findMany({
+      async (_, { page }, { client, loggedInUser }) => {
+        if (!loggedInUser) {
+          return null;
+        }
+        return await client.photo.findMany({
           where: {
             OR: [
               { user: { followers: { some: { id: loggedInUser.id } } } },
@@ -19,7 +22,8 @@ export default {
           skip: (page - 1) * 5,
 
           orderBy: { createdAt: "desc" },
-        })
+        });
+      }
     ),
   },
 };
