9e61b0752dbb0ef4878828b210f1b9eb6be0390e
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-14 00:04:16 +0900

    create type SearchPhotosResult
    Add searchPhoto,totalPhoto,totalPages

--- src/photos/searchPhotos/searchPhotos.typeDefs.ts
+++ src/photos/searchPhotos/searchPhotos.typeDefs.ts
@@ -1,7 +1,14 @@
 import { gql } from "apollo-server";
 
 export default gql`
+  type SearchPhotosResult {
+    ok: Boolean!
+    error: String
+    searchPhoto: [Photo]
+    totalPhoto: Int
+    totalPages: Int
+  }
   type Query {
-    searchPhotos(keyword: String!, lastId: Int): [Photo]!
+    searchPhotos(keyword: String!, page: Int!): SearchPhotosResult!
   }
 `;
