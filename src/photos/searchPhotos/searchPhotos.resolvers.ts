a5bc35888ef5f7ee7a57f9e5a62da2037a96c04e
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-14 00:03:15 +0900

    Add options
    add totalPhoto
    totalPhoto

--- src/photos/searchPhotos/searchPhotos.resolvers.ts
+++ src/photos/searchPhotos/searchPhotos.resolvers.ts
@@ -2,13 +2,40 @@ import { Resolvers } from "../../types";
 
 const resolvers: Resolvers = {
   Query: {
-    searchPhotos: (_, { keyword, lastId }, { client }) =>
-      client.photo.findMany({
-        where: { caption: { startsWith: keyword } },
+    searchPhotos: async (_, { keyword, page }, { client }) => {
+      const searchPhoto = await client.photo.findMany({
+        where: { caption: { contains: keyword.toLowerCase() } },
         take: 5,
-        skip: lastId ? 1 : 0,
-        ...(lastId && { cursor: { id: lastId } }),
-      }),
+        skip: (page - 1) * 5,
+      });
+
+      const totalPhoto = await client.photo.count({
+        where: { caption: { contains: keyword.toLowerCase() } },
+      });
+
+      return {
+        ok: true,
+        searchPhoto,
+        totalPhoto,
+        totalPages: Math.ceil(totalPhoto / 5),
+      };
+    },
   },
 };
+
 export default resolvers;
+
+// import { Resolvers } from "../../types";
+
+// const resolvers: Resolvers = {
+//   Query: {
+//     searchPhotos: (_, { keyword, lastId }, { client }) =>
+//       client.photo.findMany({
+//         where: { caption: { startsWith: keyword } },
+//         take: 5,
+//         skip: lastId ? 1 : 0,
+//         ...(lastId && { cursor: { id: lastId } }),
+//       }),
+//   },
+// };
+// export default resolvers;
