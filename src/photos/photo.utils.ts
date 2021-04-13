ffbe5ced6370289f0e518277db28cfa7a0ba4ca9
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-14 00:03:45 +0900

    map function works only if hashObj exists

--- src/photos/photo.utils.ts
+++ src/photos/photo.utils.ts
@@ -1,6 +1,6 @@
 export const processHashtags = (caption) => {
   const hashtags = caption.match(/#[\w]+/g) || [];
-  return hashtags.map((hashtag) => ({
+  return hashtags?.map((hashtag) => ({
     where: { hashtag },
     create: { hashtag },
   }));
