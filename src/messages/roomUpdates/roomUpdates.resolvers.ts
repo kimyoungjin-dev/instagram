d36e4b00e19eb5f6ebc90e0d189382759e07c6a0
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-15 22:24:11 +0900

    Update roomUpdates.resolvers.ts

--- src/messages/roomUpdates/roomUpdates.resolvers.ts
+++ src/messages/roomUpdates/roomUpdates.resolvers.ts
@@ -6,6 +6,8 @@ export default {
   Subscription: {
     roomUpdates: {
       //어떤것에 대해서 listen 하고, 새메시지를 받는 순간, 새로운 메시지에 대한 payload 를 받는다.
+      //pubsub의 publish 부분은 sendMessage에서 만든다.
+      //asyncIterator를 이벤트의 이름과 함께 return //이벤트의 "이름"과 "payload" 는 sendMessage에서 publish 한다.
       subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
     },
   },
