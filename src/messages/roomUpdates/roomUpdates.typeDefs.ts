ec10af42f63995ab591fbcbea42f8cc022a8ce63
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-15 22:50:34 +0900

    id: Int! is added.
    Because you shouldn't receive messages from all rooms

--- src/messages/roomUpdates/roomUpdates.typeDefs.ts
+++ src/messages/roomUpdates/roomUpdates.typeDefs.ts
@@ -2,6 +2,12 @@ import { gql } from "apollo-server";
 
 export default gql`
   type Subscription {
-    roomUpdates: Message
+    roomUpdates(id: Int!): Message
   }
 `;
+
+//id가 1인 room에서만 listen할수 있는지 알아보자
+
+//app 에서 모든 메시지의 업데이트를 listening... 할수없다. ( id를 가진 room에서만 메시지를 listening ... 할수있다.)
+
+//
