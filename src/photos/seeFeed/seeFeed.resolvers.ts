b60fb78cfa2ed950d7deccf4ea6702adc8f6c723
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-23 00:54:11 +0900

    Change variable name (page => offset)

--- src/photos/seeFeed/seeFeed.resolvers.ts
+++ src/photos/seeFeed/seeFeed.resolvers.ts
@@ -5,24 +5,21 @@ import { protectedResolver } from "../../users/users.utils";
 //"photo"를 찾을때 나의 아이디가 팔로워 목록에있는 유저의 photo를 찾으면 된다.
 export default {
   Query: {
-    seeFeed: protectedResolver(
-      async (_, { page }, { client, loggedInUser }) => {
-        if (!loggedInUser) {
-          return null;
-        }
-        return await client.photo.findMany({
-          where: {
-            OR: [
-              { user: { followers: { some: { id: loggedInUser.id } } } },
-              //userId을 사용해준다.
-              { userId: loggedInUser.id },
-            ],
-          },
-          take: 5,
-          skip: (page - 1) * 5,
-          orderBy: { createdAt: "desc" },
-        });
+    seeFeed: protectedResolver((_, { offset }, { client, loggedInUser }) => {
+      if (!loggedInUser) {
+        return null;
       }
-    ),
+      return client.photo.findMany({
+        where: {
+          OR: [
+            { user: { followers: { some: { id: loggedInUser.id } } } },
+            //userId을 사용해준다.
+            { userId: loggedInUser.id },
+          ],
+        },
+        take: 2,
+        skip: offset,
+      });
+    }),
   },
 };
--- src/photos/seeFeed/seeFeed.typeDefs.ts
+++ src/photos/seeFeed/seeFeed.typeDefs.ts
@@ -2,6 +2,6 @@ import { gql } from "apollo-server";
 
 export default gql`
   type Query {
-    seeFeed(page: Int!): [Photo]
+    seeFeed(offset: Int!): [Photo]
   }
 `;
