e9ea15eb9eb7b47c45634b881f528bd5088cc2bf
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-23 00:54:29 +0900

    Cleaning Code

--- src/photos/seePhotoLikes/seePhotoLikes.resolvers.ts
+++ src/photos/seePhotoLikes/seePhotoLikes.resolvers.ts
@@ -4,12 +4,14 @@ const resolvers: Resolvers = {
   Query: {
     seePhotoLikes: async (_, { id }, { client }) => {
       const likes = await client.like.findMany({
-        //사진의 아이디 == 인자로 받은 아이디
-        where: { photoId: id },
-        select: { user: true },
+        where: {
+          photoId: id,
+        },
+        select: {
+          user: true,
+        },
       });
-
-      return likes.map((item) => item.user);
+      return likes.map((like) => like.user);
     },
   },
 };
